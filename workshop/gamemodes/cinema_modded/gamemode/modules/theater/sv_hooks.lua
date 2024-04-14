f1e1426f38b32dab6016df1cd697d013632d21f9
PolarManne <itsme@polarmanne.xyz>
2024-04-14 12:21:27 -0700

    formatting and time fixes

--- workshop/gamemodes/cinema_modded/gamemode/modules/theater/sv_hooks.lua
+++ workshop/gamemodes/cinema_modded/gamemode/modules/theater/sv_hooks.lua
@@ -75,11 +75,11 @@ end
 function GM:PostPlayVideo( Video, Theater )
 	-- Output debug information
 	Msg("Loaded Video\n")
-	Msg("\tLocation:\t\t" .. tostring(Theater:Name()) .. "\n")
+	Msg("\tLocation:\t" .. tostring(Theater:Name()) .. "\n")
 	Msg("\tTitle:\t\t" .. tostring(Video:Title()) .. "\n")
 	Msg("\tType:\t\t" .. tostring(Video:Type()) .. "\n")
 	Msg("\tData:\t\t" .. tostring(Video:Data()) .. "\n")
-	Msg("\tTime:\t\t" .. tostring(startTime) .. "\n")
+	Msg("\tTime:\t\t" .. tostring(os.date("%H:%M:%S")) .. "\n")
 	Msg("\tDuration:\t" .. tostring(Video:Duration()) .. "\n")
 	Msg( string.format("\tRequested by %s (%s)", Video:GetOwnerName(),
 		Video:GetOwnerSteamID() ) .. "\n" )
