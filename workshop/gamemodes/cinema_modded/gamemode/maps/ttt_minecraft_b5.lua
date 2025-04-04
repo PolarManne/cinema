67635516ebb012b0715dc8867d2eb6269a8a365a
PolarManne <itsme@polarmanne.xyz>
2025-04-03 23:15:17 -0700

    new map

---
+++ workshop/gamemodes/cinema_modded/gamemode/maps/ttt_minecraft_b5.lua
@@ -0,0 +1,20 @@
+Location.Add( "ttt_minecraft_b5",
+{
+	["Nether"] =
+	{
+		Min = Vector( -1760, -1120, -1280),
+		Max = Vector( 225, 864, -800),
+		Theater = {
+			Name = "Nether",
+			Flags = THEATER_REPLICATED,
+			Pos = Vector( 11, -1054, -800 ),
+			Ang = Angle(0,180,0),
+			Width = 811,
+			Height = 455,
+			--[[ ThumbInfo = {
+				Pos = Vector( 1330, -1426, -44 ),
+				Ang = Angle(0, -124, 0),
+			} ]]
+		}
+	},
+} )
