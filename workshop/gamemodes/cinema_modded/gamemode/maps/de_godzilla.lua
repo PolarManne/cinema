2f423ace270b5905069231fd2c87d512de2ae547
PolarManne <itsme@polarmanne.xyz>
2025-10-29 10:38:29 -0700

    new map

---
+++ workshop/gamemodes/cinema_modded/gamemode/maps/de_godzilla.lua
@@ -0,0 +1,17 @@
+Location.Add( "de_godzilla",
+{
+	["City"] =
+	{
+		Min = Vector( -928, -1512, -152 ),
+		Max = Vector( 3032, 1295, 1431 ),
+
+		Theater = {
+			Name = "City",
+			Flags = THEATER_REPLICATED,
+			Pos = Vector(458, 1217, 697),
+			Ang = Angle(0,0,0),
+			Width = 1069,
+			Height = 601,
+		}
+	},
+} )
