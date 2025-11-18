e546679306be74af3e2711bc5b684ba6028b9b12
PolarManne <itsme@polarmanne.xyz>
2025-11-17 17:33:08 -0700

    more gambling

--- cinema_modded/gamemode/maps/cinema_harbl_winter.lua
+++ cinema_modded/gamemode/maps/cinema_harbl_winter.lua
@@ -46,3 +46,39 @@ Location.Add( "cinema_harbl_winter", {
 		Max = Vector( 6095, 3200, 760 ),
 	},
 } )
+
+function SpawnPokerTables()
+        if SERVER then
+                for k, v in pairs(ents.FindByClass("ent_poker_game")) do
+                        v:Remove()
+                end
+
+                local tables = {
+                        {pos = Vector(6016, 2944, 264), ang = Angle(0, 90, 0)},
+                        {pos = Vector(5928, 2983, 520), ang = Angle(0, 90, 0)},
+                        {pos = Vector(5952, 3200, 768), ang = Angle(0, 90, 0)},
+                        {pos = Vector(5175, 3080, 768), ang = Angle(0, 90, 0)},
+                        {pos = Vector(6208, 3136, 1032), ang = Angle(0, 90, 0)},
+                        {pos = Vector(6143, 3008, 2048), ang = Angle(0, 90, 0)},
+                        {pos = Vector(5152, 2336, 2792), ang = Angle(0, 90, 0)},
+                        {pos = Vector(6096, 3040, 392), ang = Angle(0, 90, 0)}
+                }
+
+                for _, config in ipairs(tables) do
+                        local table = ents.Create("ent_poker_game")
+                        table:Spawn()
+                        table:Activate()
+                        table:SetPos(config.pos)
+                        table:SetAngles(config.ang)
+                        table:SetGameState(-1)
+                        table:SetGameType(1)
+                        table:SetBetType(0)
+                        table:SetMaxPlayers(8)
+                        table:SetEntryBet(100)
+                        table:SetStartValue(1000)
+                        table:GetPhysicsObject():EnableMotion(false)
+                end
+        end
+end
+
+hook.Add( "InitPostEntity", "cinema_SpawnPokerTables", SpawnPokerTables )
