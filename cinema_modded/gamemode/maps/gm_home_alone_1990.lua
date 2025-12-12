0256eeae94e5e9362d2b793c972f43b6fc73fe93
PolarManne <itsme@polarmanne.xyz>
2025-12-12 10:42:11 -0700

    new map, rooms are finished but not all of them have screens yet

---
+++ cinema_modded/gamemode/maps/gm_home_alone_1990.lua
@@ -0,0 +1,179 @@
+Location.Add( "gm_home_alone_1990",
+{
+	["Living Room"] =
+	{
+		Volumes = {
+			{ Min = Vector(-680, -368, 48), Max = Vector(-184, -120, 192) },
+		},
+		Theater = {
+			Name = "Living Room",
+			Flags = THEATER_REPLICATED,
+			Pos = Vector( -665, -277, 169 ),
+			Ang = Angle( 0, 90, 0 ),
+			Width = 66,
+			Height = 60,
+		}
+	},
+	["Attic"] =
+	{
+		Volumes = {
+			{ Min = Vector(-663, -444, 368), Max = Vector(-200, 244, 496) },
+			{ Min = Vector(-432, -364, 208), Max = Vector(-380, -200, 368) },
+		}
+	},
+	["Master Bathroom"] =
+	{
+		Volumes = {
+			{ Min = Vector(-855, 272, 208), Max = Vector(-616, 400, 336) },
+		}
+	},
+	["Master Bedroom"] =
+	{
+		Volumes = {
+			{ Min = Vector(-608, 376, 208), Max = Vector(-256, 568, 352) },
+			{ Min = Vector(-608, 272, 208), Max = Vector(-336, 376, 352) },
+			{ Min = Vector(-432, 224, 208), Max = Vector(-336, 272, 352) },
+		}
+	},
+	["Bedroom a"] =
+	{
+		Volumes = {
+			{ Min = Vector(-328, 120, 208), Max = Vector(-184, 368, 352) },
+		}
+	},
+	["2F Hallway"] =
+	{
+		Volumes = {
+			{ Min = Vector(-432, -200, 208), Max = Vector(-336, 216, 352) },
+			{ Min = Vector(-680, -112, 199), Max = Vector(-432, 112, 352) },
+			{ Min = Vector(-336, -112, 208), Max = Vector(-256, -40, 352) },
+			{ Min = Vector(-336, -40, 208), Max = Vector(-320, 112, 352) },
+			{ Min = Vector(-856, 120, 192), Max = Vector(-440, 264, 336) },
+		}
+	},
+	["Bedroom B"] =
+	{
+		Volumes = {
+			{ Min = Vector(-680, -368, 208), Max = Vector(-440, -120, 352) },
+		}
+	},
+	["Spare Room"] =
+	{
+		Volumes = {
+			{ Min = Vector(-608, -568, 208), Max = Vector(-256, -376, 352) },
+		}
+	},
+	["Bedroom C"] =
+	{
+		Volumes = {
+			{ Min = Vector(-328, -368, 208), Max = Vector(-184, -120, 352) },
+			{ Min = Vector(-372, -368, 208), Max = Vector(-328, -208, 352) },
+		}
+	},
+	["2F Bathroom"] =
+	{
+		Volumes = {
+			{ Min = Vector(-248, -112, 208), Max = Vector(-184, 112, 336) },
+			{ Min = Vector(-311, -32, 208), Max = Vector(-248, 112, 336) },
+		}
+	},
+	["Kitchen"] =
+	{
+		Volumes = {
+			{ Min = Vector(-616, 120, 48), Max = Vector(-520, 368, 192) },
+			{ Min = Vector(-856, 120, 48), Max = Vector(-616, 631, 192) },
+			{ Min = Vector(-952, 296, 48), Max = Vector(-856, 632, 192) },
+			{ Min = Vector(-976, 296, 48), Max = Vector(-952, 440, 192) },
+		},
+
+		Theater = {
+			Name = "Kitchen",
+			Flags = THEATER_REPLICATED,
+			Pos = Vector( -852, 631.75, 176 ),
+			Ang = Angle( 0, 0, 0 ),
+			Width = 72,
+			Height = 43,
+		}
+	},
+	["Study"] =
+	{
+		Volumes = {
+			{ Min = Vector(-608, 376, 48), Max = Vector(-256, 568, 195) },
+		},
+
+		Theater = {
+			Name = "Study",
+			Flags = THEATER_REPLICATED,
+			Pos = Vector( -257, 550, 182 ),
+			Ang = Angle( 0, -90, 0 ),
+			Width = 116,
+			Height = 75,
+		}
+	},
+	["Dining Room"] =
+	{
+		Volumes = {
+			{ Min = Vector(-512, 120, 48), Max = Vector(-184, 368, 195) },
+		}
+	},
+	["TV Room"] =
+	{
+		Volumes = {
+			{ Min = Vector(-608, -568, 48), Max = Vector(-256, -376, 192) },
+		},
+
+		Theater = {
+			Name = "TV Room",
+			Flags = THEATER_REPLICATED,
+			Pos = Vector( -577, -486, 102 ),
+			Ang = Angle( 0, 90, 0 ),
+			Width = 28,
+			Height = 22,
+		}
+	},
+	["1F Hallway"] =
+	{
+		Volumes = {
+			{ Min = Vector(-680, -104, 48), Max = Vector(-188, 112, 192) },
+		}
+	},
+	["1F Bathroom"] =
+	{
+		Volumes = {
+			{ Min = Vector(-768, -96, 48), Max = Vector(-688, 112, 176) },
+		}
+	},
+	["Furnace Room"] =
+	{
+		Volumes = {
+			{ Min = Vector(-696, -360, -96), Max = Vector(-280, 112, 45) },
+			{ Min = Vector(-440, 112, -96), Max = Vector(-280, 152, 16) },
+		}
+	},
+	["Basement A"] =
+	{
+		Volumes = {
+			{ Min = Vector(-600, 160, -96), Max = Vector(-280, 528, 16) },
+			{ Min = Vector(-600, 120, -96), Max = Vector(-448, 160, 16) },
+			{ Min = Vector(-744, 120, -96), Max = Vector(-600, 312, 45) },
+		}
+	},
+	["Basement B"] =
+	{
+		Volumes = {
+			{ Min = Vector(-928, 320, -96), Max = Vector(-608, 576, 16) },
+		}
+	},
+	["Treehouse"] =
+	{
+		Volumes = {
+			{ Min = Vector(-1668, -52, 120), Max = Vector(-1524, 60, 215) },
+		}
+	},
+	["Church"] =
+	{
+		Volumes = {
+			{ Min = Vector(960, 4032, -8), Max = Vector(1765, 5307, 897) },
+		}
+	},
+} )
