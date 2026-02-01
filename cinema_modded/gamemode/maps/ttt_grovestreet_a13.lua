9b5848dd75b408da2587d7b8d2a5e5afa53feeac
PolarManne <itsme@polarmanne.xyz>
2026-02-01 16:44:14 -0700

    new map

---
+++ cinema_modded/gamemode/maps/ttt_grovestreet_a13.lua
@@ -0,0 +1,60 @@
+Location.Add( "ttt_grovestreet_a13",
+{
+	["CJ's House"] =
+	{
+		Volumes = {
+			{ Min = Vector(816, 1480, 128), Max = Vector(1312, 1983, 280) },
+		},
+		Theater = {
+			Name = "CJ's House",
+			Flags = THEATER_REPLICATED,
+			Pos = Vector( 868.25, 1503, 193.25 ),
+			Ang = Angle( 0, 135, 0 ),
+			Width = 30,
+			Height = 20,
+		}
+	},
+	["Sweet's House"] =
+	{
+		Volumes = {
+			{ Min = Vector(-144, 2976, 160), Max = Vector(40, 3296, 280) },
+		},
+		Theater = {
+			Name = "Sweet's House",
+			Flags = THEATER_REPLICATED,
+			Pos = Vector( -63, 3265, 221 ),
+			Ang = Angle( 0, 0, 0 ),
+			Width = 44,
+			Height = 31,
+		}
+	},
+	["OG Loc's House"] =
+	{
+		Volumes = {
+			{ Min = Vector(-1520, 1040, 100), Max = Vector(-1000, 1408, 224) },
+			{ Min = Vector(-1000, 1040, 100), Max = Vector(-848, 1191, 220) },
+		},
+		Theater = {
+			Name = "OG Loc's House",
+			Flags = THEATER_REPLICATED,
+			Pos = Vector( -875, 1185, 192 ),
+			Ang = Angle( 0, -90, 0 ),
+			Width = 145,
+			Height = 58,
+		}
+	},
+	["Ryder's House"] =
+	{
+		Volumes = {
+			{ Min = Vector(820, 208, 82), Max = Vector(1120, 704, 222) },
+		},
+		Theater = {
+			Name = "Ryder's House",
+			Flags = THEATER_REPLICATED,
+			Pos = Vector( 862, 508, 139 ),
+			Ang = Angle( 0, 90, 0 ),
+			Width = 48,
+			Height = 35,
+		}
+	},
+} )
