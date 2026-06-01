7fb036fc72a5c13db0394cdd3b02a7e17c783d07
PolarManne <itsme@polarmanne.xyz>
2026-06-01 12:14:51 -0700

    new map

---
+++ cinema_modded/gamemode/maps/gm_backrooms_kv31.lua
@@ -0,0 +1,163 @@
+Location.Add( "gm_backrooms_kv31",
+{
+	["room with fire hydrant"] =
+	{
+		Volumes = {
+			{ Min = Vector(7248, 432, -96), Max = Vector(8432, 1424, 96) },
+		},
+
+		Theater = {
+			Name = "room with fire hydrant",
+			Flags = THEATER_NONE,
+			Pos = Vector(8431 , 1424, 96),
+			Ang = Angle(0, -90, 0),
+			Width = 992,
+			Height = 192
+		}
+	},
+
+	["dark hallway"] =
+	{
+		Volumes = {
+			{ Min = Vector(1420, 11840, -96), Max = Vector(2412, 12096, 96) },
+		},
+
+		Theater = {
+			Name = "dark hallway",
+			Flags = THEATER_NONE,
+			Pos = Vector(2411, 12096, 96),
+			Ang = Angle(0, -90, 0),
+			Width = 192,
+			Height = 192
+		}
+	},
+
+	["green room"] =
+	{
+		Volumes = {
+			{ Min = Vector(1832, 8700, -832), Max = Vector(2088, 8900, -728) },
+		},
+
+		Theater = {
+			Name = "green room",
+			Flags = THEATER_NONE,
+			Pos = Vector(2087.75, 8900, -728),
+			Ang = Angle(0, -90, 0),
+			Width = 200,
+			Height = 98
+		}
+	},
+
+	["long bridge"] =
+	{
+		Volumes = {
+			{ Min = Vector(-7360, -640, -1024), Max = Vector(-2752, 2703, 736) },
+		},
+
+		Theater = {
+			Name = "long bridge",
+			Flags = THEATER_NONE,
+			Pos = Vector(-2754, 1712, 735),
+			Ang = Angle(0, -90, 0),
+			Width = 1376,
+			Height = 671
+		}
+	},
+
+	["corrupted room"] =
+	{
+		Volumes = {
+			{ Min = Vector(2256, 11177, -524), Max = Vector(2472, 11452, -388) },
+		},
+
+		Theater = {
+			Name = "corrupted room",
+			Flags = THEATER_NONE,
+			Pos = Vector(2256, 11451.5, -388),
+			Ang = Angle(0, 0, 0),
+			Width = 216,
+			Height = 130
+		}
+	},
+
+	["behind the ladder"] =
+	{
+		Volumes = {
+			{ Min = Vector(2304, 5248, -1089), Max = Vector(3648, 6592, -896) },
+		},
+
+		Theater = {
+			Name = "behind the ladder",
+			Flags = THEATER_NONE,
+			Pos = Vector(2847.75, 6016, -896),
+			Ang = Angle(0, -90, 0),
+			Width = 224,
+			Height = 191
+		}
+	},
+
+	["tall boy"] =
+	{
+		Volumes = {
+			{ Min = Vector(3584, 4640, -96), Max = Vector(4864, 5280, 96) },
+		},
+
+		Theater = {
+			Name = "tall boy",
+			Flags = THEATER_NONE,
+			Pos = Vector(3594, 4640.25, 96),
+			Ang = Angle(0, 180, 0),
+			Width = 10,
+			Height = 192
+		}
+	},
+
+	["portrait"] =
+	{
+		Volumes = {
+			{ Min = Vector(-1776, 9424, -876), Max = Vector(-1280, 10064, -688) },
+			{ Min = Vector(-1280, 9580, -840), Max = Vector(-564, 9720, -688) },
+		},
+
+		Theater = {
+			Name = "portrait",
+			Flags = THEATER_NONE,
+			Pos = Vector(-1775.25, 9736, -810),
+			Ang = Angle(0, 90, 0),
+			Width = 26,
+			Height = 34
+		}
+	},
+
+	["couch room"] =
+	{
+		Volumes = {
+			{ Min = Vector(-1744, 10128, -876), Max = Vector(-428, 10868, -556) },
+		},
+
+		Theater = {
+			Name = "couch room",
+			Flags = THEATER_NONE,
+			Pos = Vector(-918.25, 10324, -766),
+			Ang = Angle(0, -90, 0),
+			Width = 56,
+			Height = 42
+		}
+	},
+
+	["pool room"] =
+	{
+		Volumes = {
+			{ Min = Vector(-420, 10228, -1044), Max = Vector(196, 10896, -892) },
+		},
+
+		Theater = {
+			Name = "pool room",
+			Flags = THEATER_NONE,
+			Pos = Vector(-320, 10732, -1043.25),
+			Ang = Angle(0, 0, -90),
+			Width = 96,
+			Height = 56
+		}
+	},
+} )
