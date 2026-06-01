22aebc91a4e4474b71c2a4b5318184294587d93e
PolarManne <itsme@polarmanne.xyz>
2026-06-01 12:23:12 -0700

    forgot to scramble the names

--- cinema_modded/gamemode/maps/gm_backrooms_kv31.lua
+++ cinema_modded/gamemode/maps/gm_backrooms_kv31.lua
@@ -1,13 +1,13 @@
 Location.Add( "gm_backrooms_kv31",
 {
-	["room with fire hydrant"] =
+	["ZULU YANKEE YANKEE"] =
 	{
 		Volumes = {
 			{ Min = Vector(7248, 432, -96), Max = Vector(8432, 1424, 96) },
 		},
 
 		Theater = {
-			Name = "room with fire hydrant",
+			Name = "ZULU YANKEE YANKEE",
 			Flags = THEATER_NONE,
 			Pos = Vector(8431 , 1424, 96),
 			Ang = Angle(0, -90, 0),
@@ -16,14 +16,14 @@ Location.Add( "gm_backrooms_kv31",
 		}
 	},
 
-	["dark hallway"] =
+	["ROMEO UNIFORM HOTEL"] =
 	{
 		Volumes = {
 			{ Min = Vector(1420, 11840, -96), Max = Vector(2412, 12096, 96) },
 		},
 
 		Theater = {
-			Name = "dark hallway",
+			Name = "ROMEO UNIFORM HOTEL",
 			Flags = THEATER_NONE,
 			Pos = Vector(2411, 12096, 96),
 			Ang = Angle(0, -90, 0),
@@ -32,14 +32,14 @@ Location.Add( "gm_backrooms_kv31",
 		}
 	},
 
-	["green room"] =
+	["XRAY GOLF PAPA"] =
 	{
 		Volumes = {
 			{ Min = Vector(1832, 8700, -832), Max = Vector(2088, 8900, -728) },
 		},
 
 		Theater = {
-			Name = "green room",
+			Name = "XRAY GOLF PAPA",
 			Flags = THEATER_NONE,
 			Pos = Vector(2087.75, 8900, -728),
 			Ang = Angle(0, -90, 0),
@@ -48,14 +48,14 @@ Location.Add( "gm_backrooms_kv31",
 		}
 	},
 
-	["long bridge"] =
+	["ECHO DELTA GOLF"] =
 	{
 		Volumes = {
 			{ Min = Vector(-7360, -640, -1024), Max = Vector(-2752, 2703, 736) },
 		},
 
 		Theater = {
-			Name = "long bridge",
+			Name = "ECHO DELTA GOLF",
 			Flags = THEATER_NONE,
 			Pos = Vector(-2754, 1712, 735),
 			Ang = Angle(0, -90, 0),
@@ -64,14 +64,14 @@ Location.Add( "gm_backrooms_kv31",
 		}
 	},
 
-	["corrupted room"] =
+	["ROMEO GOLF JULIET"] =
 	{
 		Volumes = {
 			{ Min = Vector(2256, 11177, -524), Max = Vector(2472, 11452, -388) },
 		},
 
 		Theater = {
-			Name = "corrupted room",
+			Name = "ROMEO GOLF JULIET",
 			Flags = THEATER_NONE,
 			Pos = Vector(2256, 11451.5, -388),
 			Ang = Angle(0, 0, 0),
@@ -80,14 +80,14 @@ Location.Add( "gm_backrooms_kv31",
 		}
 	},
 
-	["behind the ladder"] =
+	["CHARLIE PAPA TANGO"] =
 	{
 		Volumes = {
 			{ Min = Vector(2304, 5248, -1089), Max = Vector(3648, 6592, -896) },
 		},
 
 		Theater = {
-			Name = "behind the ladder",
+			Name = "CHARLIE PAPA TANGO",
 			Flags = THEATER_NONE,
 			Pos = Vector(2847.75, 6016, -896),
 			Ang = Angle(0, -90, 0),
@@ -96,14 +96,14 @@ Location.Add( "gm_backrooms_kv31",
 		}
 	},
 
-	["tall boy"] =
+	["ALPHA YANKEE JULIET"] =
 	{
 		Volumes = {
 			{ Min = Vector(3584, 4640, -96), Max = Vector(4864, 5280, 96) },
 		},
 
 		Theater = {
-			Name = "tall boy",
+			Name = "ALPHA YANKEE JULIET",
 			Flags = THEATER_NONE,
 			Pos = Vector(3594, 4640.25, 96),
 			Ang = Angle(0, 180, 0),
@@ -112,7 +112,7 @@ Location.Add( "gm_backrooms_kv31",
 		}
 	},
 
-	["portrait"] =
+	["LIMA CHARLIE VICTOR"] =
 	{
 		Volumes = {
 			{ Min = Vector(-1776, 9424, -876), Max = Vector(-1280, 10064, -688) },
@@ -120,7 +120,7 @@ Location.Add( "gm_backrooms_kv31",
 		},
 
 		Theater = {
-			Name = "portrait",
+			Name = "LIMA CHARLIE VICTOR",
 			Flags = THEATER_NONE,
 			Pos = Vector(-1775.25, 9736, -810),
 			Ang = Angle(0, 90, 0),
@@ -129,14 +129,14 @@ Location.Add( "gm_backrooms_kv31",
 		}
 	},
 
-	["couch room"] =
+	["WHISKEY WHISKEY CHARLIE"] =
 	{
 		Volumes = {
 			{ Min = Vector(-1744, 10128, -876), Max = Vector(-428, 10868, -556) },
 		},
 
 		Theater = {
-			Name = "couch room",
+			Name = "WHISKEY WHISKEY CHARLIE",
 			Flags = THEATER_NONE,
 			Pos = Vector(-918.25, 10324, -766),
 			Ang = Angle(0, -90, 0),
@@ -145,14 +145,14 @@ Location.Add( "gm_backrooms_kv31",
 		}
 	},
 
-	["pool room"] =
+	["HOTEL YANKEE ECHO"] =
 	{
 		Volumes = {
 			{ Min = Vector(-420, 10228, -1044), Max = Vector(196, 10896, -892) },
 		},
 
 		Theater = {
-			Name = "pool room",
+			Name = "HOTEL YANKEE ECHO",
 			Flags = THEATER_NONE,
 			Pos = Vector(-320, 10732, -1043.25),
 			Ang = Angle(0, 0, -90),
